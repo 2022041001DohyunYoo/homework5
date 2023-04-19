fcc49e1a6a396edc9b594ec6006727a7a705d967
도현 유 <woomii1192@gmail.com>
2023-04-19 12:33:31 +0900

    2번째

--- circularQ.c
+++ circularQ.c
@@ -7,31 +7,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_QUEUE_SIZE 4
+#define MAX_QUEUE_SIZE 4 //큐의 최대 크기
 
-typedef char element;
+typedef char element; // element 자료형 선언
 typedef struct {
-	element queue[MAX_QUEUE_SIZE];
-	int front, rear;
-}QueueType;
+	element queue[MAX_QUEUE_SIZE]; //큐의 데이터 저장용 배열
+	int front, rear;//큐의 front와 rear 위치 저장용 변수
+}QueueType; // QueueType 자료형 선언
 
 
-QueueType *createQueue();
-int freeQueue(QueueType *cQ);
-int isEmpty(QueueType *cQ);
-int isFull(QueueType *cQ);
-void enQueue(QueueType *cQ, element item);
-void deQueue(QueueType *cQ, element* item);
-void printQ(QueueType *cQ);
-void debugQ(QueueType *cQ);
-element getElement();
+QueueType *createQueue(); //큐를 동적할당하는 함수
+int freeQueue(QueueType *cQ); //큐를 할당 해제하는 함수
+int isEmpty(QueueType *cQ); //큐가 비어있으면 1을 반환, 아니면 0을 반환하는 함수
+int isFull(QueueType *cQ); //큐가 가득 차 있다면 1, 아니면 0을 반환하는 함수
+void enQueue(QueueType *cQ, element item); //큐에 데이터를 넣는 함수
+void deQueue(QueueType *cQ, element* item); //큐에서 데이터를 꺼내는 함수
+void printQ(QueueType *cQ); //큐의 데이터를 출력하는 함수
+void debugQ(QueueType *cQ); //큐의 현재 상태와 head, rear위치를 표시하는 함수
+element getElement();//사용자 입력을 받는 함수
+
 
 int main(void)
 {
-	QueueType *cQ = createQueue();
-	element data;
+	QueueType *cQ = createQueue(); //큐 cQ 생성
+	element data; //사용자 입력 데이터 저장용 변수
 
-	char command;
+	char command; //사용자 명령 저장용 변수
 
 	do{
 		printf("\n-----------------------------------------------------\n");
@@ -40,152 +41,159 @@ int main(void)
 		printf(" Insert=i,  Delete=d,   PrintQ=p,   Debug=b,   Quit=q \n");
 		printf("------------------------------------------------------\n");
 
+		//메뉴 출력
+
 		printf("Command = ");
-		scanf(" %c", &command);
+		scanf(" %c", &command); //사용자 명령을 입력 받음
 
 		switch(command) {
 		case 'i': case 'I':
 			data = getElement();
-			enQueue(cQ, data);
+			enQueue(cQ, data); //i, I를 입력 받으면 큐에 입력받은 데이터 추가
 			break;
 		case 'd': case 'D':
-			deQueue(cQ, &data);
-			break;
+			deQueue(cQ, &data); //d, D를 입력 받으면 큐의 데이터 하나 제거
+			break; 
 		case 'p': case 'P':
-			printQ(cQ);
+			printQ(cQ); //p, P입력 받으면 현재 큐의 데이터 모두 출력
 			break;
-		case 'b': case 'B':
+		case 'b': case 'B'://b,B 입력 받으면 디버깅 실행
 			debugQ(cQ);
 			break;
 		case 'q': case 'Q':
-   	        freeQueue(cQ);
+   	        freeQueue(cQ); //q, Q 누르면 일단 큐를 할당 해제
 			break;
 		default:
-			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
+			printf("\n       >>>>>   Concentration!!   <<<<<     \n"); //명령어 이외의 문자 입력시 출력
 			break;
 		}
 
-	}while(command != 'q' && command != 'Q');
+	}while(command != 'q' && command != 'Q'); //q, Q 입력 받았을 시 반복 종료
 
 
 	return 1;
 }
 
-QueueType *createQueue()
+QueueType *createQueue() //큐를 동적할당하는 함수
 {
-	QueueType *cQ;
-	cQ = (QueueType *)malloc(sizeof(QueueType));
+	QueueType *cQ; //큐 포인터 생성
+	cQ = (QueueType *)malloc(sizeof(QueueType)); //큐 포인터에 동적할당
 	cQ->front = 0;
 	cQ->rear = 0;
 
-    for( int i = 0; i < MAX_QUEUE_SIZE; i++) cQ->queue[i] = ' ';
-	return cQ;
+	//큐의 front와 rear를 0으로 초기화
+
+    for( int i = 0; i < MAX_QUEUE_SIZE; i++) cQ->queue[i] = '\0'; //큐의 데이터 저장용 배열 초기화
+	return cQ; //cQ 주소 반환
 }
 
-int freeQueue(QueueType *cQ)
+int freeQueue(QueueType *cQ) //큐를 할당 해제하는 함수
 {
-    if(cQ == NULL) return 1;
-    free(cQ);
+    if(cQ == NULL) return 1; //만약 이미 큐가 할당 되어 있지 않다면 아래 과정 생략
+    free(cQ); //cQ 할당 해제
     return 1;
 }
 
-element getElement()
+element getElement() 
 {
-	element item;
-	printf("Input element = ");
-	scanf(" %c", &item);
-	return item;
+	element item; //사용자 입력 데이터 저장용 변수
+	printf("Input element = "); 
+	scanf(" %c", &item); //사용자 입력 받기
+	return item; //사용자 입력 데이터 반환
 }
 
 
 /* complete the function */
-int isEmpty(QueueType *cQ)
+int isEmpty(QueueType *cQ) //큐가 비어있으면 1을 반환, 아니면 0을 반환하는 함수
 {
 
-    if(cQ->front == 0 && cQ->rear == 0)    return 1;
+    if(cQ->front == 0 && cQ->rear == 0)    return 1; 
+	//만약 큐의 front와 rear가 같은 위치 가리키고 이쓰면 1반환
 
-    else return 0;
+    else return 0; // 아니면 0반환
 }
 
 /* complete the function */
-int isFull(QueueType *cQ)
+int isFull(QueueType *cQ) //큐가 가득 차 있다면 1, 아니면 0을 반환하는 함수
 {
 
-    int n = cQ->rear;
+    int n = cQ->rear; //rear의 값 임시 저장용 변수
 
-    n = (n+1)%MAX_QUEUE_SIZE;
+    n = (n+1)%MAX_QUEUE_SIZE; // 만약 rear의 바로 다음 위치가
 
-    if(n == cQ->front) return 1;
+    if(n == cQ->front) return 1; //front라면 1반환
 
-    else return 0;
+    else return 0; //아니라면 0반환
 }
 
 
 /* complete the function */
-void enQueue(QueueType *cQ, element item)
+void enQueue(QueueType *cQ, element item) //큐에 데이터를 넣는 함수
 {
 
-    if(isFull(cQ) == 0){
+    if(isFull(cQ) == 0){//큐가 가득 차 있지 않다면
           cQ->rear = (cQ->rear + 1) % MAX_QUEUE_SIZE;
+		  //rear의 위치를 다음 위치로 이동
        
-          cQ->queue[cQ->rear] = item;
+          cQ->queue[cQ->rear] = item; //이동된 rear의 위치에 데이터 넣기
 
 
 
     }
 
-    else        printf("큐가 가득 차 있습니다\n");
+    else        printf("큐가 가득 차 있습니다\n"); //큐가 가득 차 있다면 출력
     
 }
 
 /* complete the function */
-void deQueue(QueueType *cQ, element *item)
+void deQueue(QueueType *cQ, element *item) //큐에서 데이터를 꺼내는 함수
 {
     
-    if(isEmpty(cQ) == 1) printf("큐가 비어 있습니다\n");
+    if(isEmpty(cQ) == 1) printf("큐가 비어 있습니다\n"); //큐가 비어있으면 출력
 
-    else{
+    else{ //아니면
 
-       cQ->queue[cQ->front] = ' ';
 
-       cQ->front = (cQ->front + 1) %MAX_QUEUE_SIZE;
+       cQ->front = (cQ->front + 1) %MAX_QUEUE_SIZE; //front의 위치를 다음 위치로 이동하고
 
+       cQ->queue[cQ->front] = '\0'; // front 위치의 데이터를 초기화
     }
 
 }
 
 
-void printQ(QueueType *cQ)
+void printQ(QueueType *cQ) //큐의 데이터를 출력하는 함수
 {
 	int i, first, last;
 
 	first = (cQ->front + 1)%MAX_QUEUE_SIZE;
 	last = (cQ->rear + 1)%MAX_QUEUE_SIZE;
+	//first와 last는 각각 front와 rear의 다음 위치를 가리키게 함
 
-	printf("Circular Queue : [");
+	printf("Circular Queue : ["); //양식
 
-	i = first;
-	while(i != last){
-		printf("%3c", cQ->queue[i]);
-		i = (i+1)%MAX_QUEUE_SIZE;
+	i = first; //큐를 카리키는 새로운 변수 i 를 first의 위치로 맞춤
+	while(i != last){ //i가 last가 아닐 동안
+		printf("%3c", cQ->queue[i]); //(3칸으로 맞춰서) 큐의 i번째 데이터 출력
+		i = (i+1)%MAX_QUEUE_SIZE; //i를 큐의 다음위치로 이동
 	}
-	printf(" ]\n");
+	printf(" ]\n");//양식
 }
 
 
-void debugQ(QueueType *cQ)
+void debugQ(QueueType *cQ) //큐의 현재 상태와 head, rear위치를 표시하는 함수
 {
 
-	printf("\n---DEBUG\n");
-	for(int i = 0; i < MAX_QUEUE_SIZE; i++)
+	printf("\n---DEBUG\n");//양식
+	for(int i = 0; i < MAX_QUEUE_SIZE; i++)//큐의 크기 만큼 반복
 	{
-		if(i == cQ->front) {
-			printf("  [%d] = front\n", i);
-			continue;
+		if(i == cQ->front) {//만약 현재 반복수 i가 front의 값과 같다면
+			printf("  [%d] = front\n", i); //front는 i의 위치와 같다고 출력
+			continue;//아래 과정 생략
 		}
-		printf("  [%d] = %c\n", i, cQ->queue[i]);
+		printf("  [%d] = %c\n", i, cQ->queue[i]);//현재 반복수 i의 위치에 있는 큐의 데이터 출력
 
 	}
-	printf("front = %d, rear = %d\n", cQ->front, cQ->rear);
+	printf("front = %d, rear = %d\n", cQ->front, cQ->rear); //현재 front와 rear의 값 출력
 }
 
--- postfix.c
+++ postfix.c
@@ -30,18 +30,18 @@ int evalStackTop = -1; /* evalStack용 top */
 int evalResult = 0; /* 계산 결과를 저장 */
 
 
-void postfixpush(char x);
-char postfixPop();
-void evalPush(int x);
-int evalPop();
-void getInfix();
-precedence getToken(char symbol);
-precedence getPriority(char x);
-void charCat(char* c);
-void toPostfix();
-void debug();
-void reset();
-void evaluation();
+void postfixpush(char x); //postfix 스택에 push 하는 함수
+char postfixPop();//postfix 스택에서 pop하는 함수
+void evalPush(int x); //계산 스택에 push하는 함수
+int evalPop();//계산 스택에서 pop하는 함수
+void getInfix(); //infix 표기의 계산식을 입력 받는 함수
+precedence getToken(char symbol); // 문자의 토큰을 반환하는 함수
+precedence getPriority(char x);//문자의 우선순위를 반환하는 함수
+void charCat(char* c); //문자 하나를 전달받아, postfixExp에 추가하는 함수
+void toPostfix(); //postfix로 변환하는 함수
+void debug(); //디버깅용 함수
+void reset(); //초기화용 함수
+void evaluation(); //계산 함수
 
 
 
@@ -49,7 +49,7 @@ int main()
 {
 
 printf("[-----[유도현][2022041001]-----]\n\n");
-char command;
+char command; //명령 저장용 변수
 do{
 
    
@@ -58,32 +58,32 @@ do{
     printf("----------------------------------------------------------------\n");
     printf(" Infix=i, Postfix=p, Eval=e, Debug=d,Reset=r, Quit=q \n");
     printf("----------------------------------------------------------------\n");
-    printf("Command = ");
-    scanf(" %c", &command);
+    printf("Command = "); //양식
+    scanf(" %c", &command);//명령 입력 받기
     switch(command) {
     case 'i': case 'I':
-        getInfix();
+        getInfix(); //위와 같이 입력시 infix표기 계산식 입력 받기
         break;
     case 'p': case 'P':
-        toPostfix();
+        toPostfix();//위와 같이 입력시 입력 받은 계산식을 postfix로 변환
         break;
     case 'e': case 'E':
-        evaluation();
+        evaluation();//위와 같이 입력시 계산식을 계산
         break;
     case 'd': case 'D':
-        debug();
+        debug(); //위와 같이 입력시 디버깅 실행
         break;
     case 'r': case 'R':
-        reset();
+        reset(); //위와 같이 입력시 모든 스택 초기화
         break;
     case 'q': case 'Q':
         break;
 
         default:
-        printf("\n >>>>> Concentration!! <<<<<\n");
+        printf("\n >>>>> Concentration!! <<<<<\n"); //예정되지 않은 명령어 입력시 출력
         break;
     }
-}while(command != 'q' && command != 'Q');
+}while(command != 'q' && command != 'Q'); //q, Q를 입력 받았을 시 반복 종료
 
 
 return 1;
@@ -91,35 +91,38 @@ return 1;
 
 
 
-void postfixPush(char x){
+void postfixPush(char x){ //postfix 스택에 push 하는 함수
 postfixStack[++postfixStackTop] = x;
+//postfix 스택의 top부분의 위치를 하나 위로 올리고 그 위치에 전달받은 데이터 넣기
 }
 
 
 char postfixPop(){
-    char x;
+    char x; //임시저장용 변수
     if(postfixStackTop == -1)
-        return '\0';
+        return '\0';//만약 스택이 비어있으면 NULL 반환
     else {
         x = postfixStack[postfixStackTop--];
+        //아니라면 postfix 스택의 top위치의 데이터를 x에 넣고 top의 위치를 하나 아래로 
         }
-    return x;
+    return x;//x반환
 }
 
 
 
-void evalPush(int x){
+void evalPush(int x){//eval 스택에 push하는 함수
 
     evalStack[++evalStackTop] = x;
+    //eval스택의 top 부분의 위치를 하나 위로 올리고 그 위치에 전달 받은 값 넣기
 }
 
 
 int evalPop(){
 
-    if(evalStackTop == -1)
-    return -1;
-    else
-    return evalStack[evalStackTop--];
+    if(evalStackTop == -1) //top의 위치가 -1이라면
+    return -1; //아래 과정 생략, -1반환
+    else //아니라면
+    return evalStack[evalStackTop--];//top위치의 데이터 반환후 top의 위치 하나 내리기
 }
 
 
@@ -131,26 +134,26 @@ int evalPop(){
 
 void getInfix() {
     
-    printf("Type the expression >>> ");
-    scanf("%s",infixExp);
+    printf("Type the expression >>> ");//양식
+    scanf("%s",infixExp);//infix 표기의 계산식 입력 받기
 }
 
 
-precedence getToken(char symbol){
+precedence getToken(char symbol){ //symbol 의 토큰을 반환하는 함수
 
-    switch(symbol) {
-    case '(' : return lparen;
-    case ')' : return rparen;
-    case '+' : return plus;
-    case '-' : return minus;
-    case '/' : return divide;
-    case '*' : return times;
-    default : return operand;
+    switch(symbol) {//전달 받은 변수가
+    case '(' : return lparen; //(라면 lparen 반환
+    case ')' : return rparen; //)라면 rparen 반환
+    case '+' : return plus; //+라면 plus 반환
+    case '-' : return minus; //-라면 minus 반환
+    case '/' : return divide; // /라면 divide 반환
+    case '*' : return times; // *라면 times 반환
+    default : return operand; // 전부 아니라면 operand 반환
     }
 }
-precedence getPriority(char x){
+precedence getPriority(char x){//우선순위 반환 함수
 
-    return getToken(x);
+    return getToken(x);//x의 토큰을 반환
     }
 
 /**
@@ -158,10 +161,10 @@ precedence getPriority(char x){
 */
 void charCat(char* c){
 
-    if (postfixExp[0] == '\0')
-    strncpy(postfixExp, c, 1);
-    else
-    strncat(postfixExp, c, 1);
+    if (postfixExp[0] == '\0')//만약 postfix 문자열이 이 비어있다면
+    strncpy(postfixExp, c, 1);//문자열 시작 부분에 c 복사
+    else//아니라면
+    strncat(postfixExp, c, 1);//문자열이 끝나는 위치 뒤에 c 붙이기 (append)
     }
 
 /**
@@ -174,36 +177,37 @@ void toPostfix(){
     char *exp = infixExp;
     char x; /* 문자하나를 임시로 저장하기 위한 변수 */
     /* exp를 증가시켜가면서, 문자를 읽고 postfix로 변경 */
-    while(*exp != '\0')
+    while(*exp != '\0')//infix 배열이 끝날때 까지
     {
-        if(getPriority(*exp) == operand) {
-             x = *exp;
-        charCat(&x);
+        if(getPriority(*exp) == operand) { //만약 배열 요소의 우선순위가 피연산자라면
+             x = *exp; //x에 exp가 가리키는 값 넣기
+        charCat(&x);//postfix 문자열에 피연산자 집어넣기
     }
- else if(getPriority(*exp) == lparen) {
-        postfixPush(*exp);        
+ else if(getPriority(*exp) == lparen) {//만약 배열 요소의 우선순위가 좌괄호라면
+        postfixPush(*exp);  //postfix 스택에 push하기
         }        
         
-        else if(getPriority(*exp) == rparen) { 
+        else if(getPriority(*exp) == rparen) {  //만약 배열 요소 우선순위가 우괄호라면
             
-            while((x = postfixPop()) != '(') {
-                    charCat(&x);        
+            while((x = postfixPop()) != '(') {//postfix 스택에서 좌괄호가 pop 될때까지
+                    charCat(&x); //postfix 문자열에 x를 append        
             }        
         }        
         
-        else{            
-            while(getPriority(postfixStack[postfixStackTop]) >= getPriority(*exp)){            
-                x = postfixPop();            
-                charCat(&x);            
+        else{            //아니라면
+            while(getPriority(postfixStack[postfixStackTop]) >= getPriority(*exp)){   
+                    //만약 postfix 스택의 top부분의 우선순위가 infix 배열 요소의 우선순위보다 높다면
+                x = postfixPop();           
+                charCat(&x);     //postfix 스택에서 pop한 값을 postfix 문자열에 append       
                 }            
-            postfixPush(*exp);        
+            postfixPush(*exp);  //배열 요소를 postfix 스택에 push
             }        
-            exp++; 
+            exp++; //가리키는 배열 요소 위치를 하나 뒤로
             }    
             
-            while(postfixStackTop != -1){    
-                x = postfixPop();    
-                charCat(&x);    
+            while(postfixStackTop != -1){    //poxtfix 스택이 비어있을때 까지
+                x = postfixPop();   
+                charCat(&x);   //postfix 스택의 값을 pop해서 postfix 문자열에 append
             }
 
 
@@ -215,68 +219,70 @@ void toPostfix(){
 void debug()
 {
     printf("\n---DEBUG\n");
-    printf("infixExp = %s\n", infixExp);
-    printf("postExp = %s\n", postfixExp);
-    printf("eval result = %d\n", evalResult);
+    printf("infixExp = %s\n", infixExp); //infix 문자열 출력
+    printf("postExp = %s\n", postfixExp);//postfix 문자열 출력
+    printf("eval result = %d\n", evalResult); //계산 결과 출력
     printf("postfixStack : ");
     for(int i = 0; i < MAX_STACK_SIZE; i++)
         printf("%c ", postfixStack[i]);
-        printf("\n");
-    }
+        printf("\n"); 
+    }//postfix 스택의 요소를 전부 출력
 
 
-void reset(){
+void reset(){ //초기화 함수
 
-    for(int i = 0; i < MAX_STACK_SIZE; i++){
-        infixExp[i] = '\0';
-        postfixExp[i] = '\0';
-        postfixStack[i] = '\0';
-        postfixStackTop = -1;
-        evalStackTop = -1;
-        evalResult = 0;
+    for(int i = 0; i < MAX_STACK_SIZE; i++){ //최대스택크기 만큼 반복
+        infixExp[i] = '\0'; //infix 배열 초기화
+        postfixExp[i] = '\0'; //postfix 배열 초기화
+        postfixStack[i] = '\0'; //postfix 스택 초기화
+        postfixStackTop = -1; //postfix 스택의 top을 -1로 초기화
+        evalStackTop = -1; // eval 스택의 top을 -1로 초기화
+        evalResult = 0; // 계산 결과 0으로 초기화
     }
 
 }
 
-void evaluation(){
+void evaluation(){ //계산 함수
 
-    int opr1, opr2, i;
+    int opr1, opr2, i; //피연산자 1, 피연산자 2, 반복용 변수
 
-    int length = strlen(postfixExp); 
-    char symbol; 
-    evalStackTop = -1; 
+    int length = strlen(postfixExp); //길이 = postfix 문자열의 길이
+    char symbol; //문자 저장 변수
+    evalStackTop = -1; //eval 스택의 top 위치를 -1로
 
-    for(i = 0; i < length; i++) { 
-        symbol = postfixExp[i]; 
-        if(getToken(symbol) == operand){
-             evalPush(symbol - '0'); 
+    for(i = 0; i < length; i++) { //length 동안 반복
+        symbol = postfixExp[i]; //문자 = postfix 배열의 i번째 요소
+        if(getToken(symbol) == operand){ //만약 문자가 피연사자라면
+             evalPush(symbol - '0'); //eval 스택에 문자를 숫자로 변환해서 저장
         } 
         
         else { 
-            opr2 = evalPop(); 
-            opr1 = evalPop(); 
-            switch(getToken(symbol)) { 
-                case plus: 
-                evalPush(opr1 + opr2); 
+            opr2 = evalPop(); //eval 스택에서 pop한 값을 연산자 2로 설정
+            opr1 = evalPop(); //eval 스택에서 pop한 값을 연산자 1로 설정
+            switch(getToken(symbol)) { //문자의 토큰이
+                case plus: //더하기라면
+                evalPush(opr1 + opr2); //두 연산자를 더한 값을 eval 스택에 push
                 break; 
                 
-                case minus: 
+                case minus: //빼기라면
                 
-                evalPush(opr1 - opr2); 
+                evalPush(opr1 - opr2); //두 연산자를 뺀 값을 eval 스택에 push 
                 break; 
                 
-                case times: 
+                case times: //곱하기라면
                 
-                evalPush(opr1 * opr2); 
+                evalPush(opr1 * opr2); //두 연산자를 곲한 값을 eval 스택에 push
                 break; 
                 
-                case divide: 
+                case divide: //나누기라면
                 
-                evalPush(opr1 / opr2); 
-                break; default: break; 
+                evalPush(opr1 / opr2); //두 연산자를 나눈 값을 eval 스택에 push
+                break;
+                
+                default: break; 
                 } 
                 } 
                 } 
                 
-    evalResult = evalPop();
+    evalResult = evalPop(); //연산이 전부 끝난 후 eval 스택에서 pop한 값이 계산 결과
 }
